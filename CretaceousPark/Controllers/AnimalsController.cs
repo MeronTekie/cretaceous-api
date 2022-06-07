17ff7d4cf9623d282d8bd6e6124b7d0ae3621025
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-06-06 18:45:46 -0700

    Create and set API with CRUD functionality

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -15,17 +15,109 @@ namespace Cretaceous.Controllers
     {
       _db =db;
     }
+
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Animal>>> Get()
     {
-      return await _db.Animals.ToListAsync();
+      return await  _db.Animals.ToListAsync();
     }
+
     [HttpPost]
     public async Task<ActionResult<Animal>> Post(Animal animal)
     {
       _db.Animals.Add(animal);
       await _db.SaveChangesAsync();
-      return CreatedAtAction("Post",new {id =animal.AnimalId},animal);
-    }    
+      return CreatedAtAction("Post", new {id =animal.AnimalId, animal});
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Animal>> GetAnimal(int id)
+    {
+      var animal= await _db.Animals.FindAsync(id);
+      if(animal == null)
+      {
+        return NotFound();
+      }
+      return animal;
+    }
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Put( int id, Animal animal)
+    {
+      if(id!= animal.AnimalId)
+      {
+        return BadRequest();
+
+      }
+      _db.Entry(animal).State =EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch(DbUpdateConcurrencyException)
+      {
+        if(!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+
+    }
+    private bool AnimalExists( int id)
+
+    {
+      return _db.Animals.Any(model=>model.AnimalId==id);
+      
+    }
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+      if(animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+
+    }
+
+
+    // private readonly CretaceousParkContext _db;
+    // public AnimalsController(CretaceousParkContext db)
+    // {
+    //   _db =db;
+    // }
+    // [HttpGet]
+    // public async Task<ActionResult<IEnumerable<Animal>>> Get()
+    // {
+    //   return await _db.Animals.ToListAsync();
+    // }
+    // [HttpPost]
+    // public async Task<ActionResult<Animal>> Post(Animal animal)
+    // {
+    //   _db.Animals.Add(animal);
+    //   await _db.SaveChangesAsync();
+    //   return CreatedAtAction("Post",new {id =animal.AnimalId},animal);
+    // } 
+    // [HttpGet("{id}")]
+    // public async Task<ActionResult<Animal>> GetAnimal( int id) 
+    // {
+    //   var animal = await _db.Animals.FindAsync(id);
+    //   if(animal ==null)
+    //   {
+    //     return NotFound();
+    //   }
+    //   return animal;
+    // }  
+
   }
 }
--- CretaceousPark/Models/Animal.cs
+++ CretaceousPark/Models/Animal.cs
@@ -6,7 +6,6 @@ namespace CretaceousPark.Models
   public class Animal
   {
     public int AnimalId { get; set; }
-  
     public string Name { get; set; }
     public string Species { get; set; }
     public int Age { get; set; }
